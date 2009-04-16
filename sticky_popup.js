162f70748c7c3778eeafbfd7e90ad459aa4ab4b4
Tomasz Godniak <tomasz.godniak@u2i.com>
2009-04-16 16:37:40 +0200

    Added Sticky Popup code

---
+++ sticky_popup.js
@@ -0,0 +1,55 @@
+var radio_popup_window;
+
+var StickyPopup = {
+    timerId:  null,
+    parentId: null,
+    show: function(url){
+        if(StickyPopup.popupExists())
+            popup_window = window.open(url, 'Sticky_Popup', 'height=400,width=600');
+        popup_window.focus();
+        StickyPopup.addUnloadEvent();
+    },
+    popupExists: function(){
+        return (typeof(popup_window) == "undefined" || popup_window.closed)
+    },
+    addUnloadEvent: function(){
+        if(window.onunload == null) {
+            window.onunload = function(){
+                if(!StickyPopup.popupExists()) {
+                    popup_window.StickyPopup.addReference();
+                }
+            }
+            StickyPopup.parentId = StickyPopup.generateRandomString();
+        }
+    },
+    saveReference: function(){
+        StickyPopup.parentId = window.opener.StickyPopup.parentId;
+    },
+    addReference: function(){
+        var parentId = window.opener.StickyPopup.parentId;
+        StickyPopup.timerId = setInterval(function() {
+                if(StickyPopup.parentId == parentId) {
+                    window.opener.popup_window = window;
+                    parentId = window.opener.StickyPopup.parentId;
+                } else {
+                    clearInterval(StickyPopup.timerId);
+                    window.opener.StickyPopup.addUnloadEvent();
+                    StickyPopup.saveReference();
+                }
+            }, 500);
+    },
+    redirectParent: function(url){
+        window.opener.location = url;
+        window.opener.focus();
+    },
+    generateRandomString: function() {
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+        var stringLength = 12;
+        var randomString = '';
+        for (var i = 0; i < stringLength; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomString += chars.substring(rnum, rnum + 1);
+        }
+        return randomString;
+    }
+}
